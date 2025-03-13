48e6386925ad9aef68d612d9b11cd1545be7a43a
Edward Mahama <mahamaek@gmail.com>
2025-03-13 18:00:13 -0400

    add some helper methods

--- main.py
+++ main.py
@@ -1,15 +1,80 @@
-import requests
-import click
+import requests, click, sqlite3, csv
+from datetime import datetime
+
+CREATE_INVESTMENTS_SQL = """
+CREATE TABLE IF NOT EXISTS investments (
+    coin_id TEXT,
+    currency TEXT,
+    amount REAL,
+    sell INT,
+    date TIMESTAMP
+);
+"""
      
-@click.command()
-@click.option("--coin_id", default="bitcoin")
-@click.option("--currency", default = "usd")
 def get_coin_price(coin_id:str, currency:str):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={currency}"
     data = requests.get(url).json()
     coin_price = data[coin_id][currency]
+    return coin_price
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option("--coin_id", default="bitcoin")
+@click.option("--currency", default = "usd")
+def show_coin_price(coin_id:str, currency:str):
+    coin_price = get_coin_price(coin_id, currency)
     print(f"The price of {coin_id} is {coin_price:.2f} {currency.upper()}")
 
+@click.command()
+@click.option("--coin_id")
+@click.option("--currency")
+@click.option("--amount", type=float)
+@click.option("--sell", is_flag=True)
+def add_investment(coin_id, currency, amount, sell):
+    sql = "INSERT INTO investments VALUES(?,?,?,?,?);"
+    values = (coin_id, currency, amount, sell, datetime.now())
+    cur.execute(sql, values)
+    database.commit()
+
+    if sell:
+        print(f"Added sell of {amount} {coin_id}")
+    else:
+        print(f"Added buy of {amount} {coin_id}")
+
+@click.command()
+@click.option("--coin_id")
+@click.option("--currency")
+def get_investment_value(coin_id: str, currency:str):
+     coin_price = get_coin_price(coin_id, currency)
+     sql = """
+        SELECT amount FROM investments
+        WHERE coin_id =?
+        AND currency=?
+        AND sell=?;
+     """
+     buy_result = cur.execute(sql, (coin_id, currency, False)).fetchall()
+     sell_result = cur.execute(sql, (coin_id, currency, True)).fetchall()
+
+     buy_amount = sum([row[0] for row in buy_result])
+     sell_amount = sum([row[0] for row in sell_result])
+     total = buy_amount + sell_amount
+     print(f"You won a total of {total} {coin_id} worth of {total * coin_price} {currency.upper()}")
+
+def import_investments(csv_file):
+    with open(csv_file) as f:
+        rdr = csv.reader(f, delimiter=",")
+        rows = list(rdr)
+
+cli.add_command(show_coin_price)
+cli.add_command(add_investment)
+cli.add_command(get_investment_value)
 
 if __name__=="__main__":
-    get_coin_price()
+    database = sqlite3.connect("portfolio.db")
+    cur = database.cursor()
+    cur.execute(CREATE_INVESTMENTS_SQL)
+    cli()
