11fe94940786f234cff414b8a66f4683b0782526
Edward Mahama <mahamaek@gmail.com>
2025-03-17 23:00:25 -0400

    add model for investment

---
+++ ORW-Sqlalrchemy/demp.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Numeric, create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Investment(Base):
+    __tablename__= "investment"
+    id:Mapped[int] = mapped_column(primary_key=True)
+    coin: Mapped[str] = mapped_column(String(32))
+    currency: Mapped[str] = mapped_column(String(3))
+    amount: Mapped[float] = mapped_column(Numeric(5,2))
+
+    def __str__(self):
+        return f"<Investment coin: :{self.coin}, currency: {self.currency}, amount: {self.amount}>"
