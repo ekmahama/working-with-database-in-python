df3362442b08038c3254fbfa1f4ac0fed7df709e
Edward Mahama <mahamaek@gmail.com>
2025-03-18 13:53:19 -0400

    add comments

--- ORM-Sqlalrchemy/RelationShips/demp.py
+++ ORM-Sqlalrchemy/RelationShips/demp.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, Numeric, create_engine, select
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy import String, Numeric, create_engine, select, Text, ForeignKey
+from typing import List
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
 
 
 class Base(DeclarativeBase):
@@ -12,8 +13,23 @@ class Investment(Base):
     currency: Mapped[str] = mapped_column(String(3))
     amount: Mapped[float] = mapped_column(Numeric(5,2))
 
+    portfolio_id : Mapped[int] = mapped_column(ForeignKey("portfolio.id"))
+    portfolio: Mapped["Portfolio"] = relationship(back_populates="investments")
+
     def __str__(self):
         return f"<Investment coin: :{self.coin}, currency: {self.currency}, amount: {self.amount}>"
+    
+class Portfolio(Base):
+    __tablename__ = "portfolio"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(256))
+    description: Mapped[str] = mapped_column(Text())
+
+    investments: Mapped[List["Investment"]] = relationship(back_populates="portfolio")
+
+
+    def __repr__(self) -> str:
+        return f"<Portfolia name: {self.name}>"
     
 
 engine = create_engine("sqlite:///demo_r.db")
@@ -21,12 +37,21 @@ Base.metadata.create_all(engine)
 
 
 bitcoin = Investment(coin="bitcoin", currency='usd', amount = 1.0)
-ethereum = Investment(coin="ethereum", currency='gpb', amount = 2.0)
-solana = Investment(coin="solana", currency='usd', amount = 10.0)
+ethereum = Investment(coin="ethereum", currency='gpb', amount = 10.0)
+dogecoin = Investment(coin="solana", currency='usd', amount = 100.0)
 
+portfolio_1 = Portfolio(name="Portfolio 1", description="Description 1")
+portfolio_2 = Portfolio(name="Portfolio 2", description="Description 2")
 
+# Associate bitcoin with portfolio_1
+# bitcoin.portfolio = portfolio_1
+
+# Add the other investemts to portfolio_2
+portfolio_2.investments.extend([ethereum, dogecoin])
 with Session(engine) as session:
-    pass
+    #session.add(bitcoin) # NB: Adding bitcoin will add all table (portfolio) it has relation ship in db
+    session.add(portfolio_2) # Similarly , add portfolio adds it related table(investemt)
+    session.commit()
 
 
 
