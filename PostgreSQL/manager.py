0502f6db074cbb7c778793a8a2e41b1b4fcc032d
Edward Mahama <mahamaek@gmail.com>
2025-03-17 22:36:11 -0400

    fix error in name

--- PostgreSQL/manager.py
+++ PostgreSQL/manager.py
@@ -1,9 +1,9 @@
-import psycopg2, click, csv
+import psycopg2, click, csv, requests
 import psycopg2.extras
 from dataclasses import dataclass
 
 @dataclass
-class Investments:
+class Investment:
     id: int
     coin: str
     currency: str
@@ -61,6 +61,37 @@ def import_investments(filename):
 
     print(f"Added {len(rows)} Investments")
 
+@click.command()
+@click.option("--currency")
+def view_investments(currency):
+    connection = get_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    sql = """SELECT * from investments"""
+
+    if currency is not None:
+        sql += f" WHERE currency = %s;"
+        cursor.execute(sql, (currency,))
+    else:
+        cursor.execute(sql)
+
+    data = [Investment(**dict(row)) for row in cursor.fetchall()]
+
+    cursor.close()
+    connection.close()
+
+    coins = set([row.coin for row in data])
+    currencies = set([row.currency for row in data])
+
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={','.join(coins)}&vs_currencies={','.join(currencies)}"
+    coin_data = requests.get(url).json()
+
+    for investment in data:
+        coin_price = coin_data[investment.coin][investment.currency.lower()]
+        coin_total = investment.amount * coin_price
+        print(f"{investment.amount} {investment.coin} in {investment.currency} is worth {coin_total}")
+
+        
+
 
 @click.group()
 def cli():
@@ -68,6 +99,7 @@ def cli():
 
 cli.add_command(new_investment)
 cli.add_command(import_investments)
+cli.add_command(view_investments)
 
 if __name__=='__main__':
     cli()
