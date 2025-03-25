8d3f777b17ce535280fd67c286d1eb2ea4c8ad3d
Edward Mahama <mahamaek@gmail.com>
2025-03-25 18:35:32 -0400

    add util methods

--- ODM-MongeEngine/watchlists.py
+++ ODM-MongeEngine/watchlists.py
@@ -8,6 +8,27 @@ def get_coin_price(coin_id:str, currency:str):
     coin_prices = dict([(coin_id, data[coin_id][currency]) for coin_id in data])
     return coin_prices
 
+def _seed_data():
+    data = [
+        ("Bulls", "Coins to buy", "USD", [("bitcoin", "Bitcoin is number one"),("ethereum", "ethereum is number 2")]),
+        ("Bears", "Coins to sell", "USD", [("solan", "Mee..."),]),
+        ]
+
+    for row in data:
+        Watchlist(
+            name = row[0],
+            metadata = WatchListMetadata(description=row[1], currency=row[2]),
+            coins = [WatchlistCoin(coin=coin[0], note=coin) for coin in row[3]]
+        ).save()
+
+def _select_watchlist():
+    investment_coins = Watchlist.objects.all().fields(coin=1)
+    for index, coin in enumerate(investment_coins):
+        print(f"{index+1}: {coin.coin}")
+    selected_watchlist_index = int(input("Select a watchlist: ")) - 1
+    selected_watchlist_oid = investment_coins[selected_watchlist_index].id
+    return Watchlist.objects(id=selected_watchlist_oid).first()
+
 
 class WatchListMetadata(EmbeddedDocument):
     currency = fields.StringField(max_length=3)
