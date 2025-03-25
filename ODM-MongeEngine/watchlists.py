5c109fce624630c107a85dd6f935b2cccf9c80d7
Edward Mahama <mahamaek@gmail.com>
2025-03-25 18:14:30 -0400

    add watchlist classes

--- ODM-MongeEngine/watchlists.py
+++ ODM-MongeEngine/watchlists.py
@@ -7,3 +7,30 @@ def get_coin_price(coin_id:str, currency:str):
     data = requests.get(url).json()
     coin_prices = dict([(coin_id, data[coin_id][currency]) for coin_id in data])
     return coin_prices
+
+
+class WatchListMetadata(EmbeddedDocument):
+    currency = fields.StringField(max_length=3)
+    description = fields.StringField()
+    date_created = fields.DateField(default=datetime.datetime.now().date)
+
+class WatchlistCoin(EmbeddedDocument):
+    coin = fields.StringField(max_length=32)
+    note = fields.StringField()
+    date_added = fields.DateField(default=datetime.datetime.now().date)
+
+class Watchlist(Document):
+    name = fields.StringField(max_length=256)
+    metadata = fields.EmbeddedDocument(WatchListMetadata)
+    coins = fields.EmbeddedDocumentListField(WatchlistCoin)
+
+    def __str__(self):
+        return f"<Watchlist name ={self.name}, currency ={self.metadata.currency} with {len(self.coins)} coins>"
+
+
+@click.group()
+def cli():
+    pass
+
+if __name__ == "__main__":
+    cli()
